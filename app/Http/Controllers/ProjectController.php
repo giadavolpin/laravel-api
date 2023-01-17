2387d13f46485b40b46572dd8d17ad70cb1403d7
giadavolpin <volpingiada@gmail.com>
2023-01-17 16:18:25 +0100

    api

---
+++ app/Http/Controllers/ProjectController.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Project;
+use Illuminate\Support\Facades\Storage;
+
+class ProjectController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $projects = Project::all();
+        return view('admin.projects.index', compact('projects'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('admin.projects.index', compact('projects'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $data = $request->validated();
+        $slug = Project::generateSlug($request->title);
+        $data['slug'] = $slug;
+        Project::create($data);
+        return redirect()->route('admin.projects.show');
+        if ($request->hasFile('cover_image')) {
+            $path = Storage::disk('public')->put('post_images', $request->cover_image); //put per salvare img
+            $data['cover_image'] = $path;
+        }
+
+        $new_post = Post::create($data);
+        return redirect()->route('admin.posts.show', $new_post->slug);
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return view('project.show');
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        // $project->delete();
+        // return redirect()->route('admin.project.index')->with('message', "$project->title deleted seccessfully");
+    }
+}
---
+++ resources/views/guest/welcome.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.app')
+@section('content')
+
+<div class="jumbotron p-5 mb-4 bg-light">
+    <div class="container py-5">
+        <h1 class="display-5 fw-bold">
+            Welcome to Projects by Giada Volpin
+        </h1>
+
+       </div>
+</div>
+<img class="jumbotron" src=".\Users\Utente\Pictures\2015\mexico 3.2015\201.JPG" alt="">   {{-- NON FUNZIONA  --}}
+
+@endsection
+
